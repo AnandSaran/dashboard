650ae381136018f5437419d6de76929788dbc209
Anand <s.anandsaravanan@gmail.com>
2022-05-13 18:31:05 +0800

    Completed transfer UI

---
+++ app/src/main/java/com/work/dashboard/transfer/activity/TransferActivity.kt
@@ -0,0 +1,12 @@
+package com.work.dashboard.transfer.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.work.dashboard.R
+
+class TransferActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transfer)
+    }
+}
