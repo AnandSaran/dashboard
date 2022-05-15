e2961c1a09a166fcd543b982246eae4a24d34f39
Anand <s.anandsaravanan@gmail.com>
2022-05-16 00:55:37 +0800

    Code refactor

--- app/src/main/java/com/work/dashboard/transfer/activity/TransferActivity.kt
+++ app/src/main/java/com/work/dashboard/transfer/activity/TransferActivity.kt
@@ -1,12 +1,40 @@
 package com.work.dashboard.transfer.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.work.dashboard.R
+import com.work.dashboard.databinding.ActivityTransferBinding
 
 class TransferActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTransferBinding
+
+    companion object {
+        fun present(context: Context) {
+            val intent = Intent(context, TransferActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_transfer)
+        binding = ActivityTransferBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupToolbar()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setupToolbar() {
+        title = getString(R.string.transfer)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
     }
 }
